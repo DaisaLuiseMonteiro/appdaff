bec62ad41be65620d93f7ed1e6829c5e6c85e09a
Daisa Luise Monteiro <louise2002yaho@gmail.com>
2025-07-22 18:55:36 +0000

    Mon 4ième commit

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once "../vendor/autoload.php";
-require_once "../app/config/bootstrap.php";
-require_once '../routes/route.web.php';
+// require_once "../vendor/autoload.php";
+// require_once "../app/config/bootstrap.php";
+// require_once '../routes/route.web.php';
 
-use App\Core\Router;
+// use App\Core\Router;
 
 
-Router::resolve($routes);
+// Router::resolve($routes);
+echo"Is Good";
--- routes/route.web.php
+++ routes/route.web.php
@@ -6,89 +6,89 @@ use App\Controller\TransactionController;
 
 
 $routes = [
-    '/' => [
-        'controller' => SecurityController::class, 
-        'method' => 'index'
-    ],
-    '/comptePrincipal' => [
-        'controller' => CompteController::class, 
-        'method' => 'create',
-    ],
-    '/compteSecondaire' => [
-        'controller' => CompteController::class,
-        'method' => 'store',
-        'middlewares' => ['auth']
+//     '/' => [
+//         'controller' => SecurityController::class, 
+//         'method' => 'index'
+//     ],
+//     '/comptePrincipal' => [
+//         'controller' => CompteController::class, 
+//         'method' => 'create',
+//     ],
+//     '/compteSecondaire' => [
+//         'controller' => CompteController::class,
+//         'method' => 'store',
+//         'middlewares' => ['auth']
 
-    ],
-    '/compte' => [
-        'controller' => CompteController::class,
-        'method' => 'index',
-        'middlewares' => ['auth']
+//     ],
+//     '/compte' => [
+//         'controller' => CompteController::class,
+//         'method' => 'index',
+//         'middlewares' => ['auth']
 
-    ],
-    "/login" => [
-        'controller' => SecurityController::class, 
-        'method' => 'login'
-    ],
-    "/principalCreated" => [
-        'controller' => SecurityController::class,
-        'method' => 'createComptePrincipal',
-        'middlewares' => ['cryptPassword']
-    ],
-    "/secondaireCreated" => [
-        'controller' => CompteController::class,
-        'method' => 'createCompteSecondaire'
-    ],
-    "/logout" => [
-        'controller' => SecurityController::class, 
-        'method' => 'logout'
-    ],
+//     ],
+//     "/login" => [
+//         'controller' => SecurityController::class, 
+//         'method' => 'login'
+//     ],
+//     "/principalCreated" => [
+//         'controller' => SecurityController::class,
+//         'method' => 'createComptePrincipal',
+//         'middlewares' => ['cryptPassword']
+//     ],
+//     "/secondaireCreated" => [
+//         'controller' => CompteController::class,
+//         'method' => 'createCompteSecondaire'
+//     ],
+//     "/logout" => [
+//         'controller' => SecurityController::class, 
+//         'method' => 'logout'
+//     ],
 
-    '/changerTypeCompte' => [
-    'controller' => CompteController::class,
-    'method' => 'changerTypeCompte',
-    'middlewares' => ['auth'] 
-],
-  '/getComptesSecondaires' => [
-    'controller' => CompteController::class,
-    'method' => 'getComptesSecondaires',
-    'middlewares' => ['auth']
-  ],
-  '/changerEnComptePrincipal' => [
-    'controller' => CompteController::class,
-    'method' => 'changerEnComptePrincipal',
-    'middlewares' => ['auth']
-  ],
-  '/listComptes' => [
-    'controller' => CompteController::class,
-    'method' => 'show',
-    'middlewares' => ['auth']
-  ],
-  '/listTransactions' => [
-    'controller' => TransactionController::class,
-    'method' => 'index',
-    'middlewares' => ['auth']
-  ],
-  '/transactionForm' => [
-    'controller' => TransactionController::class,
-    'method' => 'create',
-    'middlewares' => ['auth']
-  ],
-  '/createTransaction' => [
-    'controller' => TransactionController::class,
-    'method' => 'store',
-    'middlewares' => ['auth']
-  ],
-  '/calculer-frais' => [
-    'controller' => TransactionController::class,
-    'method' => 'calculerFrais',
-    'middlewares' => ['auth']
-  ],
-  '/annuler-transaction' => [
-    'controller' => TransactionController::class,
-    'method' => 'annuler',
-    'middlewares' => ['auth']
-],
+//     '/changerTypeCompte' => [
+//     'controller' => CompteController::class,
+//     'method' => 'changerTypeCompte',
+//     'middlewares' => ['auth'] 
+// ],
+//   '/getComptesSecondaires' => [
+//     'controller' => CompteController::class,
+//     'method' => 'getComptesSecondaires',
+//     'middlewares' => ['auth']
+//   ],
+//   '/changerEnComptePrincipal' => [
+//     'controller' => CompteController::class,
+//     'method' => 'changerEnComptePrincipal',
+//     'middlewares' => ['auth']
+//   ],
+//   '/listComptes' => [
+//     'controller' => CompteController::class,
+//     'method' => 'show',
+//     'middlewares' => ['auth']
+//   ],
+//   '/listTransactions' => [
+//     'controller' => TransactionController::class,
+//     'method' => 'index',
+//     'middlewares' => ['auth']
+//   ],
+//   '/transactionForm' => [
+//     'controller' => TransactionController::class,
+//     'method' => 'create',
+//     'middlewares' => ['auth']
+//   ],
+//   '/createTransaction' => [
+//     'controller' => TransactionController::class,
+//     'method' => 'store',
+//     'middlewares' => ['auth']
+//   ],
+//   '/calculer-frais' => [
+//     'controller' => TransactionController::class,
+//     'method' => 'calculerFrais',
+//     'middlewares' => ['auth']
+//   ],
+//   '/annuler-transaction' => [
+//     'controller' => TransactionController::class,
+//     'method' => 'annuler',
+//     'middlewares' => ['auth']
+// ],
 
 // ],
 // '/search-transactions' => [
